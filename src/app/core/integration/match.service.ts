0b9d50dfb0b1464bac3f3e2087ed016686ef9463
jucron <jucron@gmail.com>
2024-10-04 10:52:07 +0100

    Refactor getMatches()

--- src/app/core/integration/match.service.ts
+++ src/app/core/integration/match.service.ts
@@ -3,7 +3,7 @@ import {ApiService} from "./api.service";
 import {RoutingService} from "../routing/routing.service";
 import {MatchResponse} from "../model/responses/matchResponse";
 import {Match} from "../model/match";
-import {catchError, finalize, map, Observable, of} from "rxjs";
+import {catchError, finalize, map, Observable, of, startWith} from "rxjs";
 import {AlertService} from "../alert/alert.service";
 import {MatchRequest} from "../model/requests/matchRequest";
 import {DialogService} from "../dialog/dialog.service";
@@ -28,32 +28,21 @@ export class MatchService {
     private loadingDialogService: DialogService,
   ) {
   }
-  getMatches(): Observable<MatchResponse> {
+
+  getMatches(): Observable<Match[] | null> {
     return this.apiService.getMatches().pipe(
       map(response => {
-        if (response) {
-          return response;
-        } else {
-          this.notificationService.alertGetMatchesError();
-          return this.getEmptyMatchResponse();
-        }
+        return response;
       }),
       catchError(err => {
         console.error('getMatches failed', err);
-        this.notificationService.alertGetMatchesError();
-        return of(this.getEmptyMatchResponse());
+        return of(null);
       }),
       finalize(() => {
-        this.isLoading = false;
       })
     );
   }
-  private getEmptyMatchResponse(): MatchResponse {
-    return {
-      message: 'getMatches-failed',
-      matches: [],
-    };
-  }
+
   matchRequest(matchRequest: MatchRequest) {
     this.isLoading = true;
     this.loadingDialogService.showLoadingDialog();
